213397721dda0c8418296af785c3bcf07e8da267
scotchfield <swrittenb@gmail.com>
2015-12-05 21:38:31 -0500

    First pass of _s.

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * scotchfield Theme Customizer.
+ *
+ * @package scotchfield
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function scotchfield_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'scotchfield_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function scotchfield_customize_preview_js() {
+	wp_enqueue_script( 'scotchfield_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'scotchfield_customize_preview_js' );
